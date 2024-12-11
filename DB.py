f0f1e5a5c82895d7a5aedd0b48a81c65e95b4cb7
Nathan Pham <nghiapham102603@gmail.com>
2024-12-10 22:51:33 -0800

    Moved db logon to env file

--- DB.py
+++ DB.py
@@ -1,9 +1,14 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="medication_clock"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
